69ffd7f9cd34580f35c9ba278c62a5d7c8d2e6ba
alumno <alumno>
2017-02-24 21:25:56 -0300

    ejercicio 6 y 7

---
+++ 2-InstruccionIf/jsInstruccionIF (9).1.js
@@ -0,0 +1,87 @@
+//profesor
+
+EJERCICIO 6
+
+
+Var cont=0;
+Var importe;
+
+Var max,min;
+
+Var bandera=true;
+
+While (cont <24)
+
+{
+
+Cont++;
+
+Importe=prompt (“”);
+
+While (importe <1)  //si era con if, a la segunda vez si pones negativo u otro 0 te lotoma porque es una sola vez, con WHILE ES SIEMPRE
+
+{
+
+Importe=prompt (“”);
+
+}
+
+
+If (bandera)
+
+{
+
+Bandera=false;
+
+Max=importe;
+Min=importe;
+
+}
+
+Else {
+
+If (importe >max)
+{
+Max=importe;
+}
+If (importe < min)
+{
+Min=importe;
+
+}
+
+---------------
+
+
+
+
+
+EJERCICIO 7
+
+var cont=0;
+var nota,sexo;
+
+while (cont < 100)
+
+{
+cont++;
+nota=prompt ("");
+
+while (nota    )
+
+{
+nota=prompt ("");
+
+
+}
+
+nota=parseInt(nota);
+sexo=prompt ();
+
+while (sexo    )
+
+{
+sexo=prompt ("");
+
+}
+}
